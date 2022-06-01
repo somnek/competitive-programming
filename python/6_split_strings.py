d60c3ef97c38cbe8082ae0f6d5b515b7974682fe
Som Nek <38861036+somnek@users.noreply.github.com>
2022-06-02 00:31:28 +0800

    🤯 add new solutions

--- python/6_split_strings.py
+++ python/6_split_strings.py
@@ -1,16 +1,34 @@
 def solution(s):
-    #bool(len(s)%2) * '_'
-    return s and [x for x in zip(s[::2], s[1::2])]
+  return s and [''.join(x) for x in zip(s[::2], s[1::2])] + len(s)%2 * [f"{s[-1]}_"] or []
+
+
+def sol1(s):
+  import re
+  return re.findall(".{2}", s + "_")
+
+def sol2(s):
+  result = []
+  if len(s)%2:
+    s += '_'
+  for i in range(0, len(s), 2):
+    result.append(s[i:i+2])
+  return result
+
+
+def sol3(s):
+  # "🤯"
+  return [f'{s}_'[i:i + 2] for i in range(0, len(s), 2)]
 
 
 
-print(solution('cowdone'))
-
-
-
-tests = (
-    ("asdfadsf", ['as', 'df', 'ad', 'sf']),
-    ("asdfads", ['as', 'df', 'ad', 's_']),
-    ("", []),
-    ("x", ["x_"]),
-)
+# ------------ test ------------
+try:
+  for func in [sol1, sol2, sol3, solution]:
+    assert func("cow_eat_grass") == ['co', 'w_', 'ea', 't_', 'gr', 'as', 's_']
+    assert func("asdfadsf") == ['as', 'df', 'ad', 'sf']
+    assert func("asdfads") == ['as', 'df', 'ad', 's_']
+    assert func("") == []
+    assert func("x") == ["x_"]
+  print("All tests passed 🍳")
+except AssertionError:
+  print("WRONG")
