d7f8ae91d2629642de316c37efcfa9f819d38c70
Som Nek <38861036+somnek@users.noreply.github.com>
2023-07-23 03:05:10 +0800

    git fast

--- python/7_find_the_majority.py
+++ python/7_find_the_majority.py
@@ -1,8 +1,22 @@
-def majority(l):
-    d = max(list(set([(l.count(x), x) for x in l])), key=lambda x: x[1])
-    print(d)
+def majority(arr):
+    if not arr:
+        return None
+    d = {}
+    for e in arr:
+        count = arr.count(e)
+        if count not in d:
+            d[count] = e
+        else:
+            if e in d[count]:
+                continue
+            d[count] += e
+    s = sorted(d.items())[-1]
+    return s[1][0] if len(s[1]) == 1 else None
+
 
 
 if __name__ == '__main__':
-    majority(['a', 'b', 'c', 'a'])
-    majority(['a', 'b', 'b', 'a'])
+    print(majority([]))
+    print(majority(['a', 'b', 'c']))
+    print(majority(['d', 'a', 'b', 'c', 'a']))
+    print(majority(['a', 'b', 'b', 'a']))
